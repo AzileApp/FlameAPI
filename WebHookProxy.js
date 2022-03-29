0061a935e5481eea95ef83b07ab81a2b85bcc634
JonaxDevelopment <45129587+JonaxDevelopment@users.noreply.github.com>
2022-03-29 18:10:41 +0100

    Add files via upload

--- WebHookProxy.js
+++ WebHookProxy.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const bodyParser = require('body-parser')
 const request = require('request');
-const fetch = require('node-fetch');
+const fetch = require('node-fetch')
+const db = require('quick.db');
 const app = express()
+process.on('unhandledRejection', (reason, promise) => {
+    console.log(reason.stack || reason)
+})
+
 
 app.get('/', function(req, res) {
-    res.status(200).json({ Endpoint: 'Flame', Developer: 'Jonax', Documentation: `https://docs.azile.app/reference/api-reference/flame`})
+    res.status(200).json({ Endpoint: 'Flame', Developer: 'Jonax', Documentation: `https://docs.azile.app/reference/api-reference/flame`, Requests: db.get('requests') })
 
 });
 
@@ -20,6 +25,7 @@ app.get('/api/webhooks/:id/:token', function(req, res) {
     var token = req.params.token
     request('https://discord.com/api/webhooks/' + id + "/" + token, function(error, response, body) {
         res.status(response && response.statusCode).send(body)
+        db.add('requests', 1)
     });
 
 });
@@ -37,6 +43,7 @@ app.post('/api/webhooks/:id/:token', function(req, res) {
         body: JSON.stringify(req.body)
     }).then(response => {
         res.json(req.body);
+        db.add('requests', 1)
     })
 
 });
